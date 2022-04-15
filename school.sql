ef3679647b096146c5f5bc2a812fdac799926789
dpurser-dev <david.purser@kellogg.northwestern.edu>
2022-04-15 20:18:21 +0000

    Updated in class

---
+++ school.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS teachers;
+DROP TABLE IF EXISTS courses;
+DROP TABLE IF EXISTS sections;
+DROP TABLE IF EXISTS enrollments;
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE students (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT
+);
+
+CREATE TABLE enrollments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    section_id INTEGER,
+    student_id INTEGER
+);
+
+CREATE TABLE teachers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    bio TEXT
+);
+
+CREATE TABLE courses (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    description TEXT
+);
+
+CREATE TABLE sections (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    time TEXT,
+    course_id INTEGER,
+    teacher_id INTEGER
+);
